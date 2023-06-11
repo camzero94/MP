0d49fa033a3aa202274014924e7dd31e1137b7d2
Camzero <u5500360@unimilitar.edu.co>
2023-06-11 22:05:59 +0800

    Fix Readme_v1

--- backend/server.py
+++ backend/server.py
@@ -42,19 +42,6 @@ def test_connect(data):
 def handle_message(message):
 	send(message)
 
-# @socketio.on('join')
-# def on_join(data):
-#     username = data['username']
-#     room = data['room']
-#     join_room(room)
-#     send(username + ' has entered the room.', to=room)
-
-# @socketio.on('leave')
-# def on_leave(data):
-#     username = data['username']
-#     room = data['room']
-#     leave_room(room)
-#     send(username + ' has left the room.', to=room)
 
 
 if __name__ == "__main__":
